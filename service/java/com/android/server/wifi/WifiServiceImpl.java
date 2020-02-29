d421297957bebd3fafe962ba12153d4bcb906d24
leewp14 <lee.wp14@gmail.com>
2020-03-01 04:12:30 +0800

    Revert "Wi-Fi : Abort setWifiEnabled if WifiController not ready"
    This reverts commit c4feb5a5f6fd18cb6c31c1589c46095094044e62.

--- service/java/com/android/server/wifi/WifiServiceImpl.java
+++ service/java/com/android/server/wifi/WifiServiceImpl.java
@@ -171,8 +171,6 @@ public class WifiServiceImpl extends IWifiManager.Stub {
     private WifiTrafficPoller mTrafficPoller;
     /* Tracks the persisted states for wi-fi & airplane mode */
     final WifiSettingsStore mSettingsStore;
-
-    private boolean mIsControllerStarted = false;
     /* Logs connection events and some general router and scan stats */
     private final WifiMetrics mWifiMetrics;
 
@@ -616,7 +614,6 @@ public class WifiServiceImpl extends IWifiManager.Stub {
             Log.wtf(TAG, "Failed to initialize WifiStateMachine");
         }
         mWifiController.start();
-        mIsControllerStarted = true;
 
         // If we are already disabled (could be due to airplane mode), avoid changing persist
         // state here
@@ -902,11 +899,6 @@ public class WifiServiceImpl extends IWifiManager.Stub {
                     return true;
                 }
             }
-        }
-
-        if (!mIsControllerStarted) {
-            Slog.e(TAG,"WifiController is not yet started, abort setWifiEnabled");
-            return false;
         }
 
         mWifiController.sendMessage(CMD_WIFI_TOGGLED);
